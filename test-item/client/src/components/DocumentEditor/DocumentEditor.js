781513b5377045863883156a3b3580fbe33d9c32
gechSV <gechswag@gmail.com>
2023-10-11 17:56:54 +0900

    upd

--- test-item/client/src/components/DocumentEditor/DocumentEditor.js
+++ test-item/client/src/components/DocumentEditor/DocumentEditor.js
@@ -1,30 +1,62 @@
-import React, { useContext, useEffect, useState} from 'react';
+import React, { Component, useContext, useEffect, useState, useRef} from 'react';
 import { Context } from '../../index';
 import { observer } from 'mobx-react-lite';
 import './style-document-editor.css'
 import TextEditor from './TextEditor';
+import { Document } from '../../models/DocumentSections/Document';
 
 
-function DocumentEditor(){
-    const {store} = useContext(Context);
+// function DocumentEditor(){
+//     const {store} = useContext(Context);
 
-    const [documentSections, setDocumentSections] = useState();
+//     const [documentSections, setDocumentSections] = useState();
 
-    useEffect(() => {
-        localStorage.setItem("sections", "")
-    }, [store, documentSections])
+//     useEffect(() => {
+//         localStorage.setItem("sections", "")
+//     }, [store, documentSections])
 
-    return(
+//     return(
+//         <div className='document-editor-container'> 
+//             <div className='toolbar'>
+//                 <button
+//                     className='toolbar-button'>
+//                          Добавить секцию 
+//                 </button>
+//             </div>
+//             <TextEditor/>
+//         </div>
+//     )
+// }
+
+
+class DocumentEditor extends Component{
+    constructor(props){
+        super(props)
+        this.sections = useState(new Document());
+    }
+
+    // renderSections(){
+    //     sec = this.sections.getSections()
+    //     return()
+
+    // }
+
+    render( ){
+        return (
         <div className='document-editor-container'> 
-            <div className='toolbar'>
-                <button
-                    className='toolbar-button'>
-                         Добавить секцию 
-                </button>
-            </div>
-            <TextEditor/>
-        </div>
-    )
+             <div className='toolbar'>
+                 <button
+                     className='toolbar-button'
+                     onClick={() => (this.sections.newSection(
+                        {type: "text", data: "", key: '', ref: ''}))}>
+                          Добавить секцию 
+                 </button>
+             </div>
+             {this.sections.getSections().map((el) => {
+                <TextEditor />
+             })}
+        </div>)
+    }
 }
 
-export default observer(DocumentEditor);
+export default (DocumentEditor);
--- test-item/client/src/components/DocumentEditor/TextEditor.js
+++ test-item/client/src/components/DocumentEditor/TextEditor.js
@@ -1,26 +1,60 @@
-import React, { useContext, useEffect, useState} from 'react';
+import React, {Component, useContext, useEffect, useState} from 'react';
 import { Context } from '../../index';
 import { observer } from 'mobx-react-lite';
 import './style-document-editor.css'
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState } from 'draft-js';
 
 
-function TextEditor(){
-    const {store} = useContext(Context);
+class TextEditor extends Component{
+    // const [editorState, setEditorState] = useState();
+    constructor(props){
+        super(props)
+        this.state = {editorState: EditorState.createEmpty(),};
+        this.hideToolbar = false;
+    }
 
-    const [documentData, setDocumentData] = useState();
+    onEditorStateChange = (editorState) => {
+        this.setState({
+          editorState,
+        });
+      };
 
-    useEffect(() => {
-    }, [store])
+    render(){
 
-    return(
-        <p className='editor'
-            suppressContentEditableWarning
-            contentEditable
-            spellCheck={false}
-        >
-            Edit this text
-        </p>
-    )
+        const { editorState } = this.state;
+        return(
+            <Editor
+                editorState={editorState}
+                onEditorStateChange={this.onEditorStateChange}
+                // toolbarClassName="toolbarClassName"
+                wrapperClassName="wrapperClassName"
+                editorClassName="editorClassName"
+                toolbarClassName={this.hideToolbar ? "hideToolbar" : "toolbarClassName"}
+                onFocus={() => {this.hideToolbar = false}}
+                onBlur={() => {this.hideToolbar = true}}    
+            />
+        )
+    }
+    
+    // const {store} = useContext(Context);
+
+    // const [documentData, setDocumentData] = useState();
+
+    // useEffect(() => {
+    // }, [store])
+
+    // return(
+    //     <p className='editor'
+    //         suppressContentEditableWarning
+    //         contentEditable
+    //         spellCheck={false}
+    //     >
+    //         Edit this text
+    //     </p>
+    // )
+    
 }
 
-export default observer(TextEditor);
+export default (TextEditor);
