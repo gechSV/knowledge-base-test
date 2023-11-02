a7985b61bfd2f15fe85a62a6e576beace0c837fd
gechSV <gechswag@gmail.com>
2023-11-02 17:46:03 +0900

    upd

--- test-item/client/src/components/DocumentEditor/Editor.js
+++ test-item/client/src/components/DocumentEditor/Editor.js
@@ -1,47 +1,118 @@
-import React, { Component } from 'react';
-import {documentEditorStore} from '../../index';
+// import React, { Component } from 'react';
+// import {documentEditorStore} from '../../index';
+// import './style-document-editor.css'
+// import TextEditor from './TextEditor';
+// import deleteLogo from "../../images/svg/delete.svg"
+// import { EditorState, convertToRaw } from 'draft-js';
+
+
+
+// class Editor extends Component{
+//     constructor(props){
+//         super(props);
+ 
+//         this.state = {
+//             docState: documentEditorStore.getDoc()
+//         }
+//     }
+
+//     // Добавление новой текстовой секции
+//     addNewTextSection = () => {
+//         documentEditorStore.addNewTextSection();
+//         let newState = documentEditorStore.getDoc();
+//         this.setState({docState: newState});
+//     }
+    
+//     deleteSection(index){
+//         console.log("deleteSection index", index);
+//         documentEditorStore.deleteSectionsByIndex(index);
+//         // let newState = ;
+//         this.setState({docState: documentEditorStore.getDoc()});
+//     }
+
+//     render( ){
+//         let doc = this.state.docState;
+//         // // console.log("render doc: ", )
+//         // for(let i = 0; i< doc.length; i++){
+//         //     console.log(i, '+ ', convertToRaw(doc[i].state.getCurrentContent()))
+//         // }
+//         return (
+//         <div className='document-editor-container'> 
+//              <div className='toolbar'>
+//                  <button
+//                      className='toolbar-button'
+//                      onClick={() => {this.addNewTextSection()}}>
+//                           Добавить секцию 
+//                  </button>
+//                  <button
+//                      className='toolbar-button'
+//                      onClick={() => {documentEditorStore.consoleLogAllText()}}>
+//                           Вывод в консоль
+//                  </button>
+
+//                  <button
+//                      className='toolbar-button'
+//                      onClick={() => {this.setState({docState: documentEditorStore.getDoc()});}}>
+//                           кукутвук
+//                  </button>
+
+//              </div>
+//                 {doc.map((el, i) => {
+//                     console.log(i, '+= ', convertToRaw(el.state.getCurrentContent()).blocks)
+//                    return (
+//                         <div className='textEditorCon'>
+//                             <TextEditor 
+//                                 editorIndex = {el.index} 
+//                                 editorState={el.state}/>
+//                             <button 
+//                                 className='deleteSections'
+//                                 id={'delButtonId_' + i}
+//                                 onClick={() => {this.deleteSection(i)}}
+//                                 >
+//                                 <img src={deleteLogo}>
+//                                 </img>   
+//                             </button>
+//                         </div>
+//                    )
+//                 })}
+//         </div>)
+//     }
+// }
+
+// export default (Editor);
+
+
+import React, { useContext, useState } from 'react';
+import {Context} from '../../index';
 import './style-document-editor.css'
 import TextEditor from './TextEditor';
 import deleteLogo from "../../images/svg/delete.svg"
+import { observer } from 'mobx-react-lite';
 import { EditorState, convertToRaw } from 'draft-js';
 
-
-
-class Editor extends Component{
-    constructor(props){
-        super(props);
- 
-        this.state = {
-            docState: documentEditorStore.getDoc()
-        }
-    }
+function Editor(){
+    const {documentEditorStore} = useContext(Context);
+    const [docState, setDocState] = useState(documentEditorStore.getDoc());
 
     // Добавление новой текстовой секции
-    addNewTextSection = () => {
+    function  addNewTextSection(){
         documentEditorStore.addNewTextSection();
-        let newState = documentEditorStore.getDoc();
-        this.setState({docState: newState});
+        setDocState(documentEditorStore.getDoc())
     }
     
-    deleteSection(index){
+
+    function deleteSection(index){
         console.log("deleteSection index", index);
         documentEditorStore.deleteSectionsByIndex(index);
-        // let newState = ;
-        this.setState({docState: documentEditorStore.getDoc()});
+        setDocState(documentEditorStore.getDoc());
     }
 
-    render( ){
-        let doc = this.state.docState;
-        // // console.log("render doc: ", )
-        // for(let i = 0; i< doc.length; i++){
-        //     console.log(i, '+ ', convertToRaw(doc[i].state.getCurrentContent()))
-        // }
-        return (
+    return (
         <div className='document-editor-container'> 
              <div className='toolbar'>
                  <button
                      className='toolbar-button'
-                     onClick={() => {this.addNewTextSection()}}>
+                     onClick={() => {addNewTextSection()}}>
                           Добавить секцию 
                  </button>
                  <button
@@ -52,31 +123,29 @@ class Editor extends Component{
 
                  <button
                      className='toolbar-button'
-                     onClick={() => {this.setState({docState: documentEditorStore.getDoc()});}}>
+                     onClick={() => {setDocState(documentEditorStore.getDoc())}}>
                           кукутвук
                  </button>
 
              </div>
-                {doc.map((el, i) => {
+                {docState.map((el, i) => {
                     console.log(i, '+= ', convertToRaw(el.state.getCurrentContent()).blocks)
                    return (
                         <div className='textEditorCon'>
                             <TextEditor 
-                                stateIndex = {el.index} 
+                                editorIndex = {el.index} 
                                 editorState={el.state}/>
                             <button 
                                 className='deleteSections'
-                                id={'delButtonId_' + i}
-                                onClick={() => {this.deleteSection(i)}}
-                                >
+                                onClick={() => {deleteSection(i)}}>
                                 <img src={deleteLogo}>
                                 </img>   
                             </button>
                         </div>
                    )
                 })}
-        </div>)
-    }
+        </div>
+    )
 }
 
-export default (Editor);
+export default observer(Editor);
--- test-item/client/src/components/DocumentEditor/TextEditor.js
+++ test-item/client/src/components/DocumentEditor/TextEditor.js
@@ -1,46 +1,89 @@
-import React, {Component} from 'react';
-import { documentEditorStore} from '../../index';
+// import React, {Component} from 'react';
+// import { documentEditorStore} from '../../index';
+// import './style-document-editor.css'
+// import { Editor } from "react-draft-wysiwyg";
+// import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+
+// class TextEditor extends Component{
+//     // const [editorState, setEditorState] = useState();
+//     constructor(props){
+//         super(props)
+//         this.state = {editorState : props.editorState,};
+//         this.stateIndex = this.props.stateIndex;
+//         console.log("this.stateIndex: ", this.stateIndex)
+//         this.hideToolbar = true;
+//     }
+
+//     onEditorStateChange = (editorState) => {
+//         this.setState({
+//           editorState,
+//         });
+//         documentEditorStore.setSectionByindex(this.stateIndex, this.state.editorState);
+//     };
+
+//     onChangeState(index, sections){
+//         documentEditorStore.setSectionByindex(index, sections)
+//     }
+
+//     render(){
+//         let editorState = this.state.editorState
+//         return(
+//             <Editor
+//                 editorState={editorState}
+//                 onEditorStateChange={this.onEditorStateChange}
+//                 wrapperClassName="wrapperClassName"
+//                 editorClassName="editorClassName"
+//                 toolbarClassName={this.hideToolbar ? "hideToolbar" : "toolbarClassName"}
+//                 onFocus={() => {this.hideToolbar = false}}
+//                 onBlur={() => {this.hideToolbar = true}}   
+//                 // onChange={() => {this.onChangeState(this.stateIndex, editorState)}} 
+//             />
+//         )
+//     }
+// }
+
+// export default (TextEditor);
+
+
+import React, { useContext, useEffect, useState} from 'react';
+import { Context } from '../../index';
 import './style-document-editor.css'
 import { Editor } from "react-draft-wysiwyg";
+import { observer } from 'mobx-react-lite';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 
-class TextEditor extends Component{
-    // const [editorState, setEditorState] = useState();
-    constructor(props){
-        super(props)
-        this.state = {editorState : props.editorState,};
-        this.stateIndex = this.props.stateIndex;
-        console.log("this.stateIndex: ", this.stateIndex)
-        this.hideToolbar = true;
+function TextEditor(props){
+    const {documentEditorStore} = useContext(Context); 
+
+    const [editorState, setEditorState] = useState(props.editorState);
+    const [hideToolbar, setHideToolbar] = useState(true); 
+    const editorIndex = props.editorIndex;
+
+    function onEditorStateChange(newEditorState){
+        setEditorState(newEditorState);
+        documentEditorStore.setSectionByindex(editorIndex, newEditorState);
     }
 
-    onEditorStateChange = (editorState) => {
-        this.setState({
-          editorState,
-        });
-        documentEditorStore.setSectionByindex(this.stateIndex, this.state.editorState);
-    };
+    const eonEditorStateChange = (newEditorState) => {
+            setEditorState(newEditorState)
+            documentEditorStore.setSectionByindex(this.stateIndex, newEditorState);
+        };
 
-    onChangeState(index, sections){
-        documentEditorStore.setSectionByindex(index, sections)
-    }
+    return(
+        <Editor
+            editorState={editorState}
+            onEditorStateChange={onEditorStateChange}
+            wrapperClassName="wrapperClassName"
+            editorClassName="editorClassName"
+            toolbarClassName={hideToolbar ? "hideToolbar" : "toolbarClassName"}
+            onFocus={() => {setHideToolbar(false)}}
+            onBlur={() => {setHideToolbar(true)}}   
+        />
+    )
 
-    render(){
-        let editorState = this.state.editorState
-        return(
-            <Editor
-                editorState={editorState}
-                onEditorStateChange={this.onEditorStateChange}
-                wrapperClassName="wrapperClassName"
-                editorClassName="editorClassName"
-                toolbarClassName={this.hideToolbar ? "hideToolbar" : "toolbarClassName"}
-                onFocus={() => {this.hideToolbar = false}}
-                onBlur={() => {this.hideToolbar = true}}   
-                // onChange={() => {this.onChangeState(this.stateIndex, editorState)}} 
-            />
-        )
-    }
 }
 
-export default (TextEditor);
+
+export default observer(TextEditor);
